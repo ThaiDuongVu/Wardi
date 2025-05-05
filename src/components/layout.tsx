6b41f2c79580252e8ea4fb92c2aeb0a20ed78f66
Duong Vu <vu.t.duong01@gmail.com>
2025-05-05 10:26:47 -0400

    Update footer styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,8 +2,6 @@
 
 import "@/styles/bootstrap-zephyr.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Head from "next/head";
-import Footer from "@/components/footer";
 import { useEffect } from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -14,20 +12,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div>
-      {/* Head */}
-      <Head>
-        <title>Wardi</title>
-        <meta name="description" content="AI-powered clothing try-on tool" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/* Main */}
       <main>{children}</main>
-
-      {/* Footer */}
-      <br />
-      <Footer />
     </div>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Footer from "@/components/footer";
 
 const Document = () => {
   return (
-    <Html lang="en" id="html">
+    <Html lang="en" id="html" className="h-100">
       <Head />
-      <body>
+      <body className="d-flex flex-column h-100">
+        {/* Head */}
+        <Head>
+          <title>Wardi</title>
+          <meta name="description" content="AI-powered clothing try-on tool" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        {/* Main */}
         <Main />
         <NextScript />
+
+        {/* Footer */}
+        <br />
+        <Footer />
       </body>
     </Html>
   );
